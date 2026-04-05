25fb603c204c8e3f78751e4e8dd7744ec6ef8123
SEBASTIAN GUTIERREZ GUAYACUNDO <sgutierrez78@unisalle.edu.co>
2026-04-04 22:07:18 -0500

    Implementamos la suma total de obras y la lista de espera para museos

---
+++ Ejercicio_2_Museo/com/museo/GestionMuseo.java
@@ -0,0 +1,23 @@
+package Ejercicio_2_Museo.com.museo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestionMuseo {
+    private List<ObraDeArte> inventario = new ArrayList<>();
+
+    public void agregarObra(ObraDeArte obra) {
+        inventario.add(obra);
+    }
+
+    // Requisito Director: Suma total
+    public double calcularValoracionTotal() {
+        return inventario.stream().mapToDouble(ObraDeArte::getValor).sum();
+    }
+
+    // Requisito Restaurador: Proceso diario de 5 años
+    public void procesoMantenimientoDiario() {
+        System.out.println("[SISTEMA] Ejecutando chequeo automático de 5 años...");
+        // Aquí iría la lógica de fechas
+    }
+}
